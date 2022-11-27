89f6c952ebd6226c23f56da7feba6c9fa9f78625
Wark0711 <mickey.rahul98@gmail.com>
2022-11-27 23:20:56 +0530

    Modified files to add in to-do list project

--- script.js
+++ script.js
@@ -1,105 +1,72 @@
-const days = document.querySelector('.date-section');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const currentMonth = document.querySelector('.date');
-const cDate = document.querySelector('.cdate')
-const currDate = document.getElementsByClassName('dateVal')
-const today = new Date();
-let td = new Date().toDateString();
-let month = new Date().getMonth();
-let year = new Date().getFullYear();
-wday = new Date().getDay()
-let aray = []
-let cdim
+const listItem = document.querySelector('[nList]');
+const storeItem = document.querySelector('.store')
+const btn = document.querySelector('[btn]')
+const newList = document.querySelector('[list]')
+const storingList = document.querySelector('[lists]')
+// const li = document.querySelectorAll('li')
+// let listStoringEl = []
+// let listSavingElement = []
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const weekDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-currentMonth.innerText = `${months[new Date().getMonth()]} ${new Date().getFullYear()}`
-cDate.innerText = td
-
-function addInArray(a, b, arrays) {
-    for (let i = a; i <= b; i++) {
-        arrays.push(i)
+function addList() {
+    let list = document.querySelector('[nList]').value
+    if (list.length == 0) {
+        console.warn(`Task can't be empty. Add something to it.`)
+    }
+    else {
+        // listStoringEl.push(list);
+        let li = document.createElement('li')
+        let checkbox = document.createElement('input')
+        checkbox.setAttribute('type', 'checkbox')
+        checkbox.setAttribute('class', 'check')
+        let span = document.createElement('span')
+        li.setAttribute('class', 'ilist')
+        newList.append(li)
+        li.append(checkbox)
+        li.append(span)
+        span.textContent = list;
     }
 }
 
-addInArray(1,30,aray)
-
-function getDaysInMonth(years, months) {
-    cdim = new Date(years, months, 0).getDate();
-}
-
-getDaysInMonth(year,month+1)
-
-function createDateValues(val) {
-    val.forEach(element => {
-        let div = document.createElement('div')
-        days.append(div)
-        div.setAttribute('class', 'dateVal')
-        div.innerText = element
-    });
-}
-createDateValues(weekDay)
-adjustDayDate()
-createDateValues(aray)
-
-for (let i = 0; i < weekDay.length; i++) {
-    currDate[i].style.fontWeight = 'bold'
-}
-
-function highlightDate(y,m) {
-    for (let i = 0; i < currDate.length; i++) {
-        if (currDate[i].innerText == new Date().getDate() && m === new Date().getMonth() && y === new Date().getFullYear()) {
-            currDate[i].style.backgroundColor = 'gold'
-            currDate[i].style.fontWeight = 'bold'
-            // currDate[i].style.border = '.1rem solid black'
-            break
+function removeListItems() {
+    let li = document.querySelectorAll('li')
+    for (let i = 0; i < li.length; i++) {
+        let list = li[i]
+        let elementCheck = li[i].children[0].checked;
+        if (elementCheck === true) {
+            list.remove()
         }
     }
 }
 
-highlightDate(year,month)
+function deleteListItem() {
+    // let li = document.querySelectorAll('.ilist')
+    // li = li[li.length - 1]
+    // console.log(li);
+    // li.remove()
+    // listStoringEl.pop();
+    // console.log(listStoringEl);
+    removeListItems()
+} 
 
-function removeEl(val) {
-    for (let i = 7; i < val + 7 + new Date(year,month,1).getDay(); i++) {
-        const element = currDate[7];
-        // console.log(element)
-        element.remove()
-    }  
-}
-
-prev.addEventListener('click', e => {
-    removeEl(cdim)
-    month -= 1;
-    date = new Date(year, month)
-    adjustDayDate()
-    let arais = []
-    getDaysInMonth(year,month+1)
-    addInArray(1, cdim, arais)
-    // console.log(arais)
-    createDateValues(arais)
-    currentMonth.innerText = `${months[date.getMonth()]} ${date.getFullYear()}`;
-    highlightDate(year,month)
-})
-
-next.addEventListener('click', e => {
-    removeEl(cdim)
-    month += 1;
-    date = new Date(year, month)
-    adjustDayDate()
-    let arais = []
-    getDaysInMonth(year,month+1)
-    addInArray(1, cdim, arais)
-    createDateValues(arais)
-    // console.log(arais)
-    currentMonth.innerText = `${months[date.getMonth()]} ${date.getFullYear()}`
-    highlightDate(year,month)
-})
-
-function adjustDayDate(){
-    for (let i = 0; i < new Date(year,month,1).getDay(); i++) {
-        let div = document.createElement('div')
-        days.append(div)
-        div.setAttribute('class', 'dateVal')
+function shiftListItem() {
+    let li = document.querySelectorAll('.ilist')
+    if (li.length == 0) {
+        console.warn(`Task can't be empty. Add something to it.`)
+    }
+    else {
+        // li = li[li.length - 1]
+        // li.remove()
+        // li.setAttribute('class', 'slist')
+        // storingList.append(li)
+        for (let i = 0; i < li.length; i++) {
+            let list = li[i]
+            let elementCheck = li[i].children[0].checked;
+            if (elementCheck === true) {
+                list.remove()
+                list.setAttribute('class', 'slist')
+                storingList.append(list)
+            }
+        }
     }
 }
+ 
